37a2a835b867b6d0a09aefa0c82c0beac2cc9560
Muhammad Umer <omerawan445@gmail.com>
2024-07-14 15:52:48 +0500

    Add project files.

---
+++ LinkedList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms
+{
+    public class LinkedListNode
+    {
+        public String? Data { get; set; }
+        public LinkedListNode? Next { get; set; }
+        public LinkedListNode(String Data) {
+            this.Data = Data;
+            this.Next = null;
+        }
+    }
+    public class LinkedList
+    {
+        LinkedListNode? head;
+        int count;
+        public LinkedList() {
+            head = null;
+            count = 0;
+        }
+        public void AddDataToFront(String Data)
+        {
+         LinkedListNode node= new LinkedListNode(Data);
+            node.Next = head;
+            head = node;
+        }
+        public void Display()
+        {
+            LinkedListNode? list = head;
+            while(list?.Data != null)
+            {
+                Console.WriteLine(list.Data);
+                count++;
+                list = list.Next;
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using DataStructuresAndAlgorithms;
+class DSA{   
+    public static void Main()
+    {
+        LinkedList l1 = new LinkedList();
+        l1.AddDataToFront("Umer");
+        l1.AddDataToFront("Ali");
+        l1.AddDataToFront("Ahmed");
+        l1.AddDataToFront("Saad");
+        l1.Display();
+    }
+}
